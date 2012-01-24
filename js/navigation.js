f1a02f90414911fc5f70215242d653404bad74e7
Leonty Belskiy <prishelec@gmail.com>
2012-01-24 22:51:21 +0100

    Bottom navigation

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,24 @@
 $(function() {
     $.get('navigation.inc.html', function(content) {
         $('body').append(content);
+        
+        var links = $('div#navigationContent a');
+        var count = links.size();
+        var page = 0;
+        for (var i = 0; i < count; i++) {
+            if ($(links.get(i)).prop('href') == window.location.href) {
+                page = i;
+            }
+        }
+        
+        
+     if (page != count) {
+         $("#navigationNext").html('<a href="' + $(links.get(page + 1)).attr('href') + '">' + $(links.get(page + 1)).text() + '</a>');
+     }
+     
+     if (page > 0) {
+         $("#navigationPrev").html('<a href="' + $(links.get(page - 1)).attr('href') + '">' + $(links.get(page - 1)).text() + '</a>');
+     }
+     
     });
 }); 
